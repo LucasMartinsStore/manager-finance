d39c46ff91357d9918565e7450455ad4877ff8cf
Lucas Martins <lucasifbaeuna@gmail.com>
2025-08-17 13:27:06 -0300

    feat: create component no transaction

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -4,18 +4,19 @@ import { Balance } from './components/balance/balance';
 import { TransactionItem } from './components/transaction-item/transaction-item';
 import { Transaction } from '../../shared/transaction/interface/transaction';
 import { TransactionType } from '../../shared/transaction/enums/transaction-type';
+import { NoTransactions } from './components/no-transactions/no-transactions';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance, TransactionItem],
+  imports: [Balance, TransactionItem, NoTransactions],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
   transactions = signal<Transaction[]>([
-    { title: 'Transação 1', value: 200, type: TransactionType.OUTCOME },
-    { title: 'Transação 2', value: 150, type: TransactionType.OUTCOME },
-    { title: 'Transação 3', value: 200, type: TransactionType.INCOME },
-    { title: 'Transação 4', value: 150, type: TransactionType.INCOME },
+    // { title: 'Transação 1', value: 200, type: TransactionType.OUTCOME },
+    // { title: 'Transação 2', value: 150, type: TransactionType.OUTCOME },
+    // { title: 'Transação 3', value: 200, type: TransactionType.INCOME },
+    // { title: 'Transação 4', value: 150, type: TransactionType.INCOME },
   ]);
 }
